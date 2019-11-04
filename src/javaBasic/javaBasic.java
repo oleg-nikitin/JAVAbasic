d3b13e7c51dc86fa9e05e174f55e253a4b95bee8
oleg.nikitin <oleg.nikitin@AMS-GYM-EDV1-06.gym.ams.local>
2019-11-04 18:33:22 +0100

    Java

---
+++ src/javaBasic/javaBasic.java
@@ -0,0 +1,26 @@
+package javaBasic;
+
+public class javaBasic {
+
+	public static void main(String[] args) {
+/* dasdfadfafas
+ dfsfewdgfsdgfsr
+  afdffafs
+ */
+		System.out.print("Hallo");
+			System.out.print(" ");
+				System.out.print("Du");
+				System.out.println("!");
+				System.out.println("Hallo"+" "+"Du"+"!");
+				System.out.println(1234.56);
+				System.out.println(1234.56);
+				// NICHT System.out.println(1234.56);
+				System.out.println(12+6);
+				System.out.println(12-6);
+				System.out.println(12*6);
+				System.out.println(12/3F);
+				System.out.println("1 + 1 = " + (1 + 1));
+				
+	}
+
+}
