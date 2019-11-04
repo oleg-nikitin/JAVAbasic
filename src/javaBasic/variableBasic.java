7eef5f8ffaaa22d7eeb9ebd7499fb79ae3355224
oleg.nikitin <oleg.nikitin@AMS-GYM-EDV1-06.gym.ams.local>
2019-11-04 18:49:12 +0100

    Variablen

---
+++ src/javaBasic/variableBasic.java
@@ -0,0 +1,29 @@
+package javaBasic;
+
+public class variableBasic {
+
+	public static void main(String[] args) {
+		// Boolean = 1 Bit
+		boolean wahr_oder_falsch;
+		wahr_oder_falsch = true;
+				System.out.println(wahr_oder_falsch);
+		wahr_oder_falsch = false;
+				System.out.println(wahr_oder_falsch);
+		// Byte
+				byte Kurzzahl = 1;
+				System.out.println(Kurzzahl);
+				System.out.println(Byte.MAX_VALUE);
+				System.out.println(Byte.MIN_VALUE);
+		//byte = 2bit
+				short Zahl = 12;
+				System.out.println(Zahl);
+				System.out.println(Short.MAX_VALUE);
+				System.out.println(Short.MIN_VALUE);
+		// int - 4 byte
+				int Zahle = 123456;
+				System.out.println(Zahle);
+				System.out.println(Integer.MAX_VALUE);
+				System.out.println(Integer.MIN_VALUE);
+	}
+
+}
