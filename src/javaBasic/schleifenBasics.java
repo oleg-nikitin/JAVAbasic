c84484930034406e9ca314b84f3f8a43a79784ac
oleg.nikitin <oleg.nikitin@AMS-GYM-EDV1-06.gym.ams.local>
2019-11-11 18:16:36 +0100

    schleif for

---
+++ src/javaBasic/schleifenBasics.java
@@ -0,0 +1,18 @@
+package javaBasic;
+
+public class schleifenBasics {
+
+	public static void main(String[] args) {
+		// FOR schleifd
+		for (int i = 0; i < 8; i = i+1)
+		System.out.println(i);
+		//
+		System.out.println("------");
+		int j=1;
+		for (;j<10;) {
+			System.out.println(j);
+			j = j+1;
+		}
+	}
+
+}
