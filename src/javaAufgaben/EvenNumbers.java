686d83b6db9a2c026bd3a40a3db836f60a5bad84
oleg.nikitin <oleg.nikitin@AMS-GYM-EDV1-06.gym.ams.local>
2019-11-18 18:16:10 +0100

    Odd/even numbers

---
+++ src/javaAufgaben/EvenNumbers.java
@@ -0,0 +1,18 @@
+package javaAufgaben;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		for (int k = -2; k <= 98;) { 
+			k = k + 2;
+			if
+			(k%2<1);	
+			
+					
+						System.out.println(k);
+			}
+					
+	
+
+}
+}
---
+++ src/javaAufgaben/OddNumbers.java
@@ -0,0 +1,14 @@
+package javaAufgaben;
+
+public class OddNumbers {
+
+	public static void main(String[] args) {
+		for (int k = -1; k <= 99;) { 
+			k = k + 2;
+					
+						System.out.println(k);
+					
+					}
+	}
+
+}
