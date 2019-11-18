a48e168c65c394662ece8544825fcccfb67144c4
oleg.nikitin <oleg.nikitin@AMS-GYM-EDV1-06.gym.ams.local>
2019-11-18 18:31:32 +0100

    Eingabe

---
+++ src/javaBasic/EingabeBasics.java
@@ -0,0 +1,12 @@
+package javaBasic;
+
+import java.util.Scanner;
+
+public class EingabeBasics {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+				System.out.print(scanner.nextLine());
+	}
+
+}
